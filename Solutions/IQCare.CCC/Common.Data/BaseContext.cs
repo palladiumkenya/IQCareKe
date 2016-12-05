155f87d26935051833bf9c5b17a08e51958ffa94
Koske Kimutai <koskedk@live.com>
2016-12-05 15:44:18 +0300

    Refactored to add Common.Data

---
+++ Solutions/IQCare.CCC/Common.Data/BaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using DataAccess.Base;
+
+namespace Common.Data
+{
+    public abstract class BaseContext : DbContext
+    {
+        public BaseContext() : base((DbConnection)DataMgr.GetConnection(), true)
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
