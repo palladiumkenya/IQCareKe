ebf562e9069bb34cca1c92889dd9eca48db23fc0
sosewe <stephen.osewe@live.com>
2017-01-05 16:48:45 +0300

    baseline Entities

---
+++ IQCare.CCC/Entities.CCC/Baseline/PatientArtUseHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Entities.CCC.Baseline
+{
+    [Serializable]
+
+    [Table("ARTUseHistory")]
+   public class PatientArtUseHistory
+    {
+        [Column]
+
+        public int PatientId { get; set; }
+        [ForeignKey("PatientId")]
+        public int PatientMasterVisitId { get; set; }
+        [ForeignKey("PatientMasterVisitId")]
+        public string TreatmentType { get; set; }
+        public string Purpose { get; set; }
+        public string Regimen { get; set; }
+        public DateTime DateLastUsed { get; set; }
+    }
+}
---
+++ IQCare.CCC/Entities.CCC/Baseline/PatientDiagnosis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Entities.CCC.Baseline
+{
+    [Serializable]
+    [Table("PatientDiagnosis")]
+    public class PatientDiagnosis
+    {
+        [Column]
+
+        public virtual int PatientId { get; set; }
+        [ForeignKey("PatientId")]
+        public virtual int PatientMasterVisitId { get; set; }
+        [ForeignKey("PatientMasterVisitId")]
+        public int Diagnosis { get; set; }
+        public string ManagementPlan { get; set; }
+    }
+}
