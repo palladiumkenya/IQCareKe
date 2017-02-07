556ee57f2b6b7585cab42bf7681907054d74e160
sosewe <stephen.osewe@live.com>
2017-02-07 22:18:21 +0300

    Baseline Business process implementation

---
+++ IQCare.CCC/Interface.CCC/Baseline/IPatientHivEnrolmetBaselineManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Entities.CCC.Baseline;
+
+namespace Interface.CCC.Baseline
+{
+    public interface IPatientHivEnrolmetBaselineManager
+    {
+        int AddPatientHivEnrollment(PatientHivEnrollmentBaseline patientHivEnrollmentBaseline);
+        int UpdatePatientHivEnrollment(PatientHivEnrollmentBaseline patientHivEnrollmentBaseline);
+        int DeletePatientHivEnrollment(int id);
+        List<PatientHivEnrollmentBaseline> GetPatientHivEnrollmentBaselines(int patientId);
+    }
+}
