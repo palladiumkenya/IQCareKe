9c8368603c9eaaacf611035a841c2d8462a487bb
GMwasi <mwasi.brian@thepalladiumgroup.com>
2017-02-08 10:39:53 +0300

    Appointment forms structure complete

--- IQCare.CCC/IQCare.Web.CCC/CCC/Appointment/ScheduleAppointment.aspx.cs
+++ IQCare.CCC/IQCare.Web.CCC/CCC/Appointment/ScheduleAppointment.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Application.Presentation;
+using Interface.Scheduler;
 
 namespace IQCare.Web.CCC.Appointment
 {
@@ -11,7 +14,47 @@ namespace IQCare.Web.CCC.Appointment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                this.PopulateDropDown();
+            }
+        }
 
+        private void PopulateDropDown()
+        {
+            //*******Get the patient details on the basis of Patient Enrollment Id and show the details.*******//
+            var formManager = (IAppointment)ObjectFactory.CreateInstance("BusinessProcess.Scheduler.BAppointment, BusinessProcess.Scheduler");
+            var theDtSet = formManager.GetAppointmentStatus();
+
+            BindFunctions appBind = new BindFunctions();
+            IQCareUtils theUtils = new IQCareUtils();
+
+            appBind.BindCombo(status, theDtSet.Tables[0], "Name", "Id", "Id");
+            ListItem item = status.Items.FindByText("Pending");
+            if (item != null)
+            {
+                item.Selected = true;
+            }
+            else
+            {
+                status.ClearSelection();
+            }
+
+            DataSet theDtSetPurpose = formManager.GetAppointmentReasons(0);
+
+            DataView theDv = new DataView(theDtSetPurpose.Tables[0]);
+            theDv.RowFilter = "DeleteFlag=0";
+            DataTable TheDT = (DataTable)theUtils.CreateTableFromDataView(theDv);
+            appBind.BindCombo(Reason, TheDT, "Name", "Id");
+            theDv.Dispose();
+            TheDT.Clear();
+
+            appBind = new BindFunctions();
+            DataTable dt = (((DataTable)Session["AppModule"]).DefaultView).ToTable(true, "ModuleName", "ModuleId");
+            dt.DefaultView.RowFilter = "ModuleName Not In ('PM/SCM')";
+            appBind.BindCombo(ServiceArea, dt, "ModuleName", "ModuleId", "ModuleName");
+
+            ServiceArea.ClearSelection();
         }
     }
 }
