42e290fe835ac4fe5299e8f8d33cbbfc88431195
sosewe <stephen.osewe@live.com>
2017-02-20 15:55:22 +0300

    merged and greencard datatabase documentaton

--- IQCare.CCC/IQCare.Web.CCC/CCC/Appointment/ScheduleAppointment.aspx.cs
+++ IQCare.CCC/IQCare.Web.CCC/CCC/Appointment/ScheduleAppointment.aspx.cs
@@ -12,19 +12,6 @@ namespace IQCare.Web.CCC.Appointment
 {
     public partial class ScheduleAppointment : System.Web.UI.Page
     {
-        
-        protected int PatientId
-        {
-            get { return Convert.ToInt32(Session["patientId"]); } 
-
-        }
-
-        protected int PatientMasterVisitId
-        {
-            get { return Convert.ToInt32(Session["patientMasterVisitId"]); }
-        }
-
-        private IPatientMasterVisitManager _visitManager = (IPatientMasterVisitManager)ObjectFactory.CreateInstance("BusinessProcess.CCC.visit.BPatientmasterVisit, BusinessProcess.CCC");
         public int PatientId;
         public int PatientMasterVisitId;
         private readonly IPatientMasterVisitManager _visitManager = (IPatientMasterVisitManager)ObjectFactory.CreateInstance("BusinessProcess.CCC.visit.BPatientmasterVisit, BusinessProcess.CCC");
@@ -70,8 +57,8 @@ namespace IQCare.Web.CCC.Appointment
 
         private void GetSessionDetails()
         {
-           // PatientId = Convert.ToInt32(HttpContext.Current.Session["PatientId"]);
-           // PatientMasterVisitId = Convert.ToInt32(HttpContext.Current.Session["patientMasterVisitId"]);
+            PatientId = Convert.ToInt32(HttpContext.Current.Session["PatientId"]);
+            PatientMasterVisitId = Convert.ToInt32(HttpContext.Current.Session["PatientMasterVisitId"]);
             if (PatientMasterVisitId == 0)
             {
                 PatientMasterVisit visit = new PatientMasterVisit()
