13fe0ce639a23f1be94f571c8782ebbc659288a8
John Muritu Macharia <johnmuritu@gmail.com>
2017-02-20 20:18:00 +0300

    issue with tfs

---
+++ BusinessProcess/BusinessProcess.CCC/Baseline/BPatientHivEnrollmentBaselineManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Base;
+using DataAccess.CCC.Context;
+using DataAccess.CCC.Repository;
+using Entities.CCC.Baseline;
+using Interface.CCC.Baseline;
+
+namespace BusinessProcess.CCC.Baseline
+{
+
+    public class BPatientHivEnrollmentBaselineManager:ProcessBase,IPatientHivEnrolmetBaselineManager
+    {
+        private readonly UnitOfWork _unitOfWork = new UnitOfWork(new GreencardContext());
+        internal int Result;
+
+        public int AddPatientHivEnrollment(PatientHivEnrollmentBaseline patientHivEnrollmentBaseline)
+        {
+            _unitOfWork.PatientHivEnrollmentBaselineRepository.Add(patientHivEnrollmentBaseline);
+            return Result = _unitOfWork.Complete();
+        }
+
+        public int UpdatePatientHivEnrollment(PatientHivEnrollmentBaseline patientHivEnrollmentBaseline)
+        {
+            _unitOfWork.PatientHivEnrollmentBaselineRepository.Update(patientHivEnrollmentBaseline);
+            return Result = _unitOfWork.Complete();
+        }
+
+        public int DeletePatientHivEnrollment(int id)
+        {
+            var patientHivEnrollmentBaseline = _unitOfWork.PatientHivEnrollmentBaselineRepository.GetById(id);
+            _unitOfWork.PatientHivEnrollmentBaselineRepository.Remove(patientHivEnrollmentBaseline);
+            return Result = _unitOfWork.Complete();
+        }
+
+        public List<PatientHivEnrollmentBaseline> GetPatientHivEnrollmentBaselines(int patientId)
+        {
+            var patientHivEnrollment = _unitOfWork.PatientHivEnrollmentBaselineRepository.FindBy(x => x.PatientId == patientId & !x.DeleteFlag)
+                    .OrderByDescending(x => x.Id)
+                    .Distinct()
+                    .Take(1)
+                    .ToList();
+            return patientHivEnrollment;
+        }
+    }
+}
---
+++ BusinessProcess/BusinessProcess.CCC/Baseline/BPatientVaccination.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Base;
+using Interface.CCC.Baseline;
+using System;
+using Entities.CCC.Encounter;
+using DataAccess.CCC.Repository;
+using DataAccess.CCC.Context;
+
+namespace BusinessProcess.CCC.Baseline
+{
+    public class BPatientVaccination : ProcessBase, IPatientVaccinationManager
+    {
+        private readonly UnitOfWork _unitOfWork = new UnitOfWork(new GreencardContext());
+        internal int Result;
+
+        public int addPatientVaccination(PatientVaccination patientVaccination)
+        {
+            _unitOfWork.PatientVaccinationRepository.Add(patientVaccination);
+            Result = _unitOfWork.Complete();
+            return Result;
+        }
+
+        public int DeletePatientVaccination(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int updatePatientVaccination(PatientVaccination patientVaccination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
