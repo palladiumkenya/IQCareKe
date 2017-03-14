9dc936d33d849ec41829af52a8b77f093c74bec0
Patrick.Wangoo <Patrick.Wangoo@thepalladiumgroup.com>
2017-03-14 12:00:14 +0300

    vitals graph with bug fix

--- IQCare.CCC/IQCare.Web.CCC/CCC/Patient/PatientHome.aspx.cs
+++ IQCare.CCC/IQCare.Web.CCC/CCC/Patient/PatientHome.aspx.cs
@@ -16,28 +16,11 @@ namespace IQCare.Web.CCC.Patient
         public int PatientMasterVisitId;
         public decimal march_height;
 
-        IPatientVitals _vitals =
-            (IPatientVitals) ObjectFactory.CreateInstance("BusinessProcess.CCC.BPatientVitals, BusinessProcess.CCC");
+      
 
         protected void Page_Load(object sender, EventArgs e)
         {
             
-            patientId = Convert.ToInt32(HttpContext.Current.Session["PatientId"]);
-
-
-            List<PatientVital> vitals = _vitals.GetCurrentPatientVital(patientId);
-            if (vitals != null || vitals.Count > 0)
-            {
-                //vitals.FirstOrDefault(l => l.CreateDate.Month == )
-                foreach (var item in vitals)
-                {
-                    march_height = item.CreateDate.Month;
-                   // var height = item.Height;
-                }
-
-                //march_height;
-                //feb_height;
-            }
-        }
+         }
     }
 }
