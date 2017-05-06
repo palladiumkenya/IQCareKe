7989f7eb4ccce819a92959c6b86e78c21cadd2e1
sosewe <stephenosewe@gmail.com>
2017-05-06 23:49:13 +0300

    patienthome and arvtreatmenttracker entity

---
+++ IQCare.CCC/Entities.CCC/Lookup/PatientArvTreamentTrackerLookup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Entities.CCC.Lookup
+{
+    [Serializable]
+    [Table("ARVTreatmentTracker")]
+    class PatientArvTreamentTrackerLookup
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int ServiceAreaId { get; set; }
+        public int PatientMasterVisitId { get; set; }
+        public DateTime? RegimenStartDate { get; set; }
+        public int RegimenId { get; set; }
+        public string Regimen { get; set; }
+        public int RegimenLineId { get; set; }
+        public string RegimenLine { get; set; }
+        public int? DrugId { get; set; }
+        public DateTime? RegimenStatusDate { get; set; }
+        public int TreatmentStatusId { get; set; }
+        public int TreatmentStatusReasonId { get; set; }
+
+    }
+}
