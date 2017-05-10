dea9e51ffc010f2629fa16e7a918cd890f4e1f3c
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2017-05-10 10:17:52 +0300

    bug fixes

--- IQCare.CCC/IQCare.Web.CCC/CCC/OneTimeEvents/OneTimeEventsTracker.aspx.cs
+++ IQCare.CCC/IQCare.Web.CCC/CCC/OneTimeEvents/OneTimeEventsTracker.aspx.cs
@@ -14,9 +14,13 @@ namespace IQCare.Web.CCC.OneTimeEvents
                 var patientLookUpManager = new PatientLookupManager();
                 var patientlookup = patientLookUpManager.GetPatientDetailSummary(patientId);
                 DateTime dob = patientlookup.DateOfBirth;
+                DateTime enrollmentDateTime = patientlookup.EnrollmentDate;
+
                 int age = new DateTime(DateTime.Now.Subtract(dob).Ticks).Year - 1;
 
                 Age.Value = age.ToString();
+                this.dob.Value = dob.ToString();
+                this.enrollmentDate.Value = enrollmentDateTime.ToString();
             }
             catch (Exception exception)
             {
