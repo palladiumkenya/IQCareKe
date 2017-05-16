8da4ed29a0a69a8ee641faff361171d625c64672
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2017-05-16 17:43:32 +0300

    Bug fixes green card, blue card

---
+++ IQCare.CCC/BusinessProcess.CCC/Lookup/BPersonGreenCardLookupManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Base;
+using DataAccess.CCC.Context;
+using DataAccess.CCC.Repository;
+using Entities.CCC.Lookup;
+using Interface.CCC.Lookup;
+
+namespace BusinessProcess.CCC.Lookup
+{
+    public class BPersonGreenCardLookupManager : ProcessBase, IPersonGreenCardLookupManager
+    {
+        public List<PersonGreenCardLookup> GetPtnPkByPersonId(int personId)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(new GreencardContext()))
+            {
+                var list = unitOfWork.PersonGreenCardLookupRepository.FindBy(x => x.PersonId == personId).ToList();
+                unitOfWork.Dispose();
+                return list;
+            }
+        }
+    }
+}
---
+++ IQCare.CCC/Interface.CCC/Lookup/IPersonGreenCardLookupManager.cs
@@ -0,0 +1,13 @@
+﻿using Entities.CCC.Lookup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface.CCC.Lookup
+{
+    public interface IPersonGreenCardLookupManager
+    {
+        List<PersonGreenCardLookup> GetPtnPkByPersonId(int personId);
+    }
+}
