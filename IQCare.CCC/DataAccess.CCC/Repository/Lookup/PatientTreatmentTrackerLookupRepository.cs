a008e8fc8ff9441a33a8f93c47b555d1b065f71c
sosewe <stephenosewe@gmail.com>
2017-05-19 17:49:54 +0300

    adding drug interuptions check

--- IQCare.CCC/DataAccess.CCC/Repository/Lookup/PatientTreatmentTrackerLookupRepository.cs
+++ IQCare.CCC/DataAccess.CCC/Repository/Lookup/PatientTreatmentTrackerLookupRepository.cs
@@ -26,7 +26,7 @@ namespace DataAccess.CCC.Repository.Lookup
         {
             
             var patientRegimen =
-                _context.PatientTreamentTrackerLookups.Where(x => x.PatientId == patientId & x.TreatmentStatus == "Start Treatment" || x.TreatmentStatus == "DrugSwitches" || x.TreatmentStatus == "Continue current treatment" || x.TreatmentStatus == "Drug Substitution" || x.TreatmentStatus == "Drug Interactions")
+                _context.PatientTreamentTrackerLookups.Where(x => x.PatientId == patientId & x.TreatmentStatus == "Start Treatment" || x.TreatmentStatus == "DrugSwitches" || x.TreatmentStatus == "Continue current treatment" || x.TreatmentStatus == "Drug Substitution" || x.TreatmentStatus == "Drug Interruptions")
                     .OrderByDescending(x => x.Id)
                     .FirstOrDefault();
             return patientRegimen;
@@ -35,7 +35,7 @@ namespace DataAccess.CCC.Repository.Lookup
         public PatientTreamentTrackerLookup GetPatientbaselineRegimenLookup(int patientId)
         {
             var patientRegimen =
-                 _context.PatientTreamentTrackerLookups.Where(x => x.PatientId == patientId & x.TreatmentStatus == "Start Treatment" || x.TreatmentStatus == "DrugSwitches" || x.TreatmentStatus == "Continue current treatment" || x.TreatmentStatus == "Drug Substitution" || x.TreatmentStatus == "Drug Interactions")
+                 _context.PatientTreamentTrackerLookups.Where(x => x.PatientId == patientId & x.TreatmentStatus == "Start Treatment" || x.TreatmentStatus == "DrugSwitches" || x.TreatmentStatus == "Continue current treatment" || x.TreatmentStatus == "Drug Substitution" || x.TreatmentStatus == "Drug Interruptions")
                      .OrderBy(x => x.Id)
                      .FirstOrDefault();
             return patientRegimen;
