17c5fa1cc694fe4ef08db52579098fc31c6be8c3
sosewe <stephenosewe@gmail.com>
2017-07-06 07:48:38 +0300

    version changes

--- Application/Application.Presentation/GblIQCare.cs
+++ Application/Application.Presentation/GblIQCare.cs
@@ -25,10 +25,10 @@ namespace Application.Presentation
 
         public GblIQCare()
         {
-            AppVersion = "Ver 1.0.0.1 Kenya HMIS";
-            ReleaseDate = "01-May-2017";
-            VersionName = "Kenya HMIS Ver 1.0.0.1";
-            DbVersion = "Ver 1.0.0.1 Kenya HMIS";
+            AppVersion = "Ver 1.0.0.2 Kenya HMIS";
+            ReleaseDate = "01-June-2017";
+            VersionName = "Kenya HMIS Ver 1.0.0.2";
+            DbVersion = "Ver 1.0.0.2 Kenya HMIS";
         }
         public static int iFormMode;
 
