258b389b0bcd3fe07e16fde078f9f7dd4e958243
Brian Mwasi <mwasi.brian@thepalladiumgroup.com>
2017-07-21 11:24:25 +0300

    patient categorization logic complete

---
+++ IQCare.CCC/BusinessProcess.CCC/BPatientCategorization.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.CCC.Context;
+using DataAccess.CCC.Repository;
+using Entities.CCC.Encounter;
+using System.Linq;
+
+namespace BusinessProcess.CCC
+{
+    public class BPatientCategorization
+    {
+        private int _result;
+
+        public int AddPatientCategorization(PatientCategorization p)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(new GreencardContext()))
+            {
+                unitOfWork.PatientCategorizationRepository.Add(p);
+                _result = unitOfWork.Complete();
+                unitOfWork.Dispose();
+                return _result;
+            }
+        }
+
+        public PatientCategorization GetPatientCategorization(int id)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(new GreencardContext()))
+            {
+                var consent = unitOfWork.PatientCategorizationRepository.FindBy(x => x.PatientId == id)
+                    .OrderBy(x => x.Id)
+                    .FirstOrDefault();
+                unitOfWork.Dispose();
+                return consent;
+            }
+        }
+
+        public void DeletePatientCategorization(int id)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(new GreencardContext()))
+            {
+                var categorization = unitOfWork.PatientCategorizationRepository.GetById(id);
+                unitOfWork.PatientCategorizationRepository.Remove(categorization);
+                _result = unitOfWork.Complete();
+                unitOfWork.Dispose();
+            }
+        }
+
+        public int UpdatePatientCategorization(PatientCategorization p)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(new GreencardContext()))
+            {
+                var categorization = new PatientCategorization()
+                {
+                    Categorization = p.Categorization,
+                    DateAssessed = p.DateAssessed
+                };
+                unitOfWork.PatientCategorizationRepository.Update(categorization);
+                _result = unitOfWork.Complete();
+                unitOfWork.Dispose();
+                return _result;
+            }
+        }
+    }
+}
---
+++ IQCare.CCC/Interface.CCC/IPatientCategorizationmanager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities.CCC.Encounter;
+
+namespace Interface.CCC
+{
+    public interface IPatientCategorizationManager
+    {
+        int AddPatientCategorization(PatientCategorization p);
+        PatientCategorization GetPatientCategorization(int id);
+        void DeletePatientCategorization(int id);
+        int UpdatePatientCategorization(PatientCategorization p);
+    }
+}
