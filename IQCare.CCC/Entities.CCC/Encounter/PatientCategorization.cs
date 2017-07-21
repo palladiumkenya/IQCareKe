355c9b5c13cf1ab6acc0fb11607234959fc6cd19
Brian Mwasi <mwasi.brian@thepalladiumgroup.com>
2017-07-21 10:41:58 +0300

    created patient categorization entity and table

---
+++ IQCare.CCC/Entities.CCC/Encounter/PatientCategorization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using Entities.CCC.Enrollment;
+using Entities.Common;
+
+namespace Entities.CCC.Encounter
+{
+    [Serializable]
+    [Table("PatientCategorization")]
+    class PatientCategorization : BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        [ForeignKey("PatientId")]
+        public virtual PatientEntity Patient { get; set; }
+        public int PatientMasterVisitId { get; set; }
+        public PatientCategorizationStatus Categorization { get; set; }
+        public DateTime DateAssessed { get; set; }
+    }
+
+    public enum PatientCategorizationStatus
+    {
+        Stable,
+        Unstable
+    }
+}
