64bb7c7a214384d075934f8018bfbde31c01f3de
Joseph Njung'e <joesnjunge@live.com>
2017-08-03 13:21:15 +0300

    Task 10007

--- IQCare.CCC/DataAccess.CCC/Repository/Patient/PatientAppointmentRepository.cs
+++ IQCare.CCC/DataAccess.CCC/Repository/Patient/PatientAppointmentRepository.cs
@@ -25,6 +25,7 @@ namespace DataAccess.CCC.Repository.Patient
         public List<PatientAppointment> GetByPatientId(int patientId)
         {
             IPatientAppointmentRepository patientAppointmentRepository = new PatientAppointmentRepository();
+            
             List<PatientAppointment> patientAppointment = patientAppointmentRepository.FindBy(p => p.PatientId == patientId).ToList();
             return patientAppointment;
         }
@@ -35,11 +36,17 @@ namespace DataAccess.CCC.Repository.Patient
             List<PatientAppointment> patientAppointment = patientAppointmentRepository.FindBy(p => DbFunctions.TruncateTime(p.AppointmentDate) == DbFunctions.TruncateTime(date)).ToList();
             return patientAppointment;
         }
+        public List<AppointmentSummary> GetAppointmentSummaryByDate(DateTime date)
+        {
+         //   IPatientAppointmentRepository patientAppointmentRepository = new PatientAppointmentRepository();
+           return _context.AppointmentSummary.Where(p => DbFunctions.TruncateTime(p.AppointmentDate) == DbFunctions.TruncateTime(date)).ToList();
 
+        }
         public List<PatientAppointment> GetByDateRange(DateTime startDate, DateTime endDate)
         {
             IPatientAppointmentRepository patientAppointmentRepository = new PatientAppointmentRepository();
-            List<PatientAppointment> patientAppointment = patientAppointmentRepository.FindBy(p => p.AppointmentDate >= DbFunctions.TruncateTime(startDate) && p.AppointmentDate<= DbFunctions.TruncateTime(endDate)).ToList();
+            List<PatientAppointment> patientAppointment = patientAppointmentRepository
+                .FindBy(p => p.AppointmentDate >= DbFunctions.TruncateTime(startDate) && p.AppointmentDate<= DbFunctions.TruncateTime(endDate)).ToList();
             return patientAppointment;
         }
        
--- IQCare.CCC/IQCare.CCC.UILogic/TestingSummaryStatisticsManager.cs
+++ IQCare.CCC/IQCare.CCC.UILogic/TestingSummaryStatisticsManager.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Application.Presentation;
+﻿using Application.Presentation;
 using Entities.CCC.Lookup;
 using Interface.CCC.Lookup;
+using System;
+using System.Collections.Generic;
 
 namespace IQCare.CCC.UILogic
 {
