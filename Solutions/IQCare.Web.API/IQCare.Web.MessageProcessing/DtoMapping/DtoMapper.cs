4240b6a86f2ce84e7d1ec7deda9df956a186f48c
Brian Mwasi <mwasi.brian@thepalladiumgroup.com>
2017-09-19 22:36:18 +0300

    patient registration incoming message handling

---
+++ Solutions/IQCare.Web.API/IQCare.Web.MessageProcessing/DtoMapping/DtoMapper.cs
@@ -0,0 +1,106 @@
+﻿using IQCare.DTO;
+using System;
+using IQCare.Web.MessageProcessing.JsonMappingEntities;
+
+namespace IQCare.Web.MessageProcessing.DtoMapping
+{
+    public class DtoMapper : IDtoMapper
+    {
+        public Registration PatientRegistrationMapping(PatientRegistrationEntity entity)
+        {
+            var registration = new Registration()
+            {
+                MotherMaidenName = entity.PATIENT_IDENTIFICATION.MOTHER_MAIDEN_NAME,
+                MaritalStatus = entity.PATIENT_IDENTIFICATION.MARITAL_STATUS,
+                County = entity.PATIENT_IDENTIFICATION.PATIENT_ADDRESS.PHYSICAL_ADDRESS.COUNTY,
+                SubCounty = entity.PATIENT_IDENTIFICATION.PATIENT_ADDRESS.PHYSICAL_ADDRESS.SUB_COUNTY,
+                Ward = entity.PATIENT_IDENTIFICATION.PATIENT_ADDRESS.PHYSICAL_ADDRESS.WARD,
+                Village = entity.PATIENT_IDENTIFICATION.PATIENT_ADDRESS.PHYSICAL_ADDRESS.VILLAGE,
+                DateOfDeath = entity.PATIENT_IDENTIFICATION.DEATH_DATE,
+                DeathIndicator = entity.PATIENT_IDENTIFICATION.DEATH_INDICATOR,
+
+            };
+            return registration;
+        }
+
+        public void PatientTransferIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatedClientInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PatientTransferOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegimenChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopDrugs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrugPrescriptionRaised()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrugOrderCancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrugOrderFulfilment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentScheduling()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentUpdated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentRescheduling()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentCanceled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentHonored()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UniquePatientIdentification()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViralLoadLabOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViralLoadResults()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solutions/IQCare.Web.API/IQCare.Web.MessageProcessing/DtoMapping/IDtoMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IQCare.DTO;
+using IQCare.Web.MessageProcessing.JsonMappingEntities;
+
+namespace IQCare.Web.MessageProcessing.DtoMapping
+{
+    public interface IDtoMapper
+    {
+        Registration PatientRegistrationMapping(PatientRegistrationEntity entity);
+        void PatientTransferIn();
+        void UpdatedClientInformation();
+        void PatientTransferOut();
+        void RegimenChange();
+        void StopDrugs();
+        void DrugPrescriptionRaised();
+        void DrugOrderCancel();
+        void DrugOrderFulfilment();
+        void AppointmentScheduling();
+        void AppointmentUpdated();
+        void AppointmentRescheduling();
+        void AppointmentCanceled();
+        void AppointmentHonored();
+        void UniquePatientIdentification();
+        void ViralLoadLabOrder();
+        void ViralLoadResults();
+    }
+}
