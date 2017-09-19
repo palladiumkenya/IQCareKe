c5ea9c17246c202b44a70ee4d3eb7737fc378a4a
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2017-09-19 14:55:47 +0300

    1. implement registration

--- IQCare.CCC/IQCare.CCC.UILogic/Interoperability/ProcessRegistration.cs
+++ IQCare.CCC/IQCare.CCC.UILogic/Interoperability/ProcessRegistration.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IQCare.DTO;
 
 namespace IQCare.CCC.UILogic.Interoperability
 {
-    class ProcessRegistration
+    public class ProcessRegistration : IInteropDTO<DTO.Registration>
     {
+        public Registration Retrive(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Save(Registration t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Registration t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ IQCare.CCC/IQCare.DTO/IInteropDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IQCare.DTO
+{
+    public interface IInteropDTO <T> where T : class
+    {
+        string Save(T t);
+        T Retrive(int entityId);
+
+        string Update(T t);
+    }
+}
