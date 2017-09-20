b1a14a63a01e8020cc975d51fc8c9bd22afb31b3
Joseph Njung'e <joesnjunge@live.com>
2017-09-20 21:21:13 +0300

    Send data

--- IQCare.CCC/IQCare.DTO/IlMessage.cs
+++ IQCare.CCC/IQCare.DTO/IlMessage.cs
@@ -9,33 +9,37 @@ namespace IQCare.DTO
     //    public IlMessageType MessageType { get; set; }
     //    public string EventOccurred { get; set; }
     //}
-    public class IlMessageEventArgs: EventArgs
-    {
+    //public interface IDataExchange
+    //{
+    //   event InteropEventHandler OnDataExchage;
+    //}
+    //public class IlMessageEventArgs: EventArgs
+    //{
 
-        public int PatientId { get; set; }
-        public int EntityId { get; set; }
-        public IlMessageType MessageType { get; set; }
-        public string EventOccurred { get; set; }
-    }
-    public delegate void InteropEventHandler(IlMessageEventArgs e);
-    public enum IlMessageType
-    {
-        NewClientRegistration,
-        PatientTransferIn,
-        UpdatedClientInformation,
-        PatientTransferOut,
-        RegimenChange,
-        StopDrugs,
-        DrugPrescriptionRaised,
-        DrugOrderCancel,
-        DrugOrderFulfilment,
-        AppointmentScheduling,
-        AppointmentUpdated,
-        AppointmentRescheduling,
-        AppointmentCanceled,
-        AppointmentHonored,
-        UniquePatientIdentification,
-        ViralLoadLabOrder,
-        ViralLoadResults
-    }
+    //    public int PatientId { get; set; }
+    //    public int EntityId { get; set; }
+    //    public IlMessageType MessageType { get; set; }
+    //    public string EventOccurred { get; set; }
+    //}
+    //public delegate void InteropEventHandler(IlMessageEventArgs e);
+    //public enum IlMessageType
+    //{
+    //    NewClientRegistration,
+    //    PatientTransferIn,
+    //    UpdatedClientInformation,
+    //    PatientTransferOut,
+    //    RegimenChange,
+    //    StopDrugs,
+    //    DrugPrescriptionRaised,
+    //    DrugOrderCancel,
+    //    DrugOrderFulfilment,
+    //    AppointmentScheduling,
+    //    AppointmentUpdated,
+    //    AppointmentRescheduling,
+    //    AppointmentCanceled,
+    //    AppointmentHonored,
+    //    UniquePatientIdentification,
+    //    ViralLoadLabOrder,
+    //    ViralLoadResults
+   // }
 }
