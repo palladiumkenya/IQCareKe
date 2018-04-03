ad14a3ddfc12c5c05a1dccf6fa3db835004abdc4
Brian Mwasi <mwasi.brian@thepalladiumgroup.com>
2018-04-03 12:42:47 +0300

    resolved breaking code

--- Interfaces/Interface.Pharmacy/IPharmacyDispense.cs
+++ Interfaces/Interface.Pharmacy/IPharmacyDispense.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Entities.Pharmacy;
 
 namespace Interface.Pharmacy
 {
     public interface IPharmacyDispense
     {
-        string DispensePrescription(int prescription, Dispense)
+        string DispensePrescription(int prescription, Dispense dispense);
     }
 }
