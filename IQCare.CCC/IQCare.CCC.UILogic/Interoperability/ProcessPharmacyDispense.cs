18acdc69a3d6a5e106c11345da6bf1bed863fc88
Brian Mwasi <mwasi.brian@thepalladiumgroup.com>
2018-04-03 11:53:36 +0300

    process pharmacy dispense partially done

--- IQCare.CCC/IQCare.CCC.UILogic/Interoperability/ProcessPharmacyDispense.cs
+++ IQCare.CCC/IQCare.CCC.UILogic/Interoperability/ProcessPharmacyDispense.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Application.Presentation;
 using Entities.CCC.Lookup;
+using Entities.CCC.pharmacy;
 using Interface.CCC.Pharmacy;
 using IQCare.DTO;
 
@@ -53,13 +55,79 @@ namespace IQCare.CCC.UILogic.Interoperability
                 var dispensedDrugs = _pharmacyDispenseManager.GetByPharmacyOrderId(pharmacyOrder.ptn_pharmacy_pk);
                 if (dispensedDrugs != null)
                 {
+                    var newDispensedDrugs = dispensedDrugs;
+                    foreach (var drug in newDispensedDrugs)
+                    {
+                        //todo get drugnames for drugId
+                        string drugname = "";
+                        var messageDispensed = drugDispensed.PHARMACY_DISPENSE.FirstOrDefault(x=>x.DRUG_NAME ==drugname);
+                        if (messageDispensed != null)
+                        {
+                            drug.DispensedQuantity = messageDispensed.QUANTITY_DISPENSED;
+                            //todo get frequencyId 
+                            //drug.FrequencyID = messageDispensed.FREQUENCY;
+                            drug.PatientInstructions = messageDispensed.DISPENSING_NOTES;
+                            //todo get strength ids
+                            //drug.StrengthID = messageDispensed.STRENGTH;
+                            drug.Duration = messageDispensed.DURATION;
+                            drug.UpdateDate = DateTime.Now;
+                            drug.UserID = 1;
+                            drug.SingleDose = Convert.ToDecimal(Regex.Replace(messageDispensed.DOSAGE, @"^[A-Za-z]+", ""));
 
+                        }
+                        try
+                        {
+                            _pharmacyDispenseManager.UpdatePatientPharmacyDispense(drug);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            return Msg = "error " + e.Message;
+                        }
+                    }
                 }
                 else
                 {
-                    
+                    PatientPharmacyDispense newlyDispensedDrugs = new PatientPharmacyDispense();
+                    foreach (var drug in drugDispensed.PHARMACY_DISPENSE)
+                    {
+                        //todo get drug ids from drug names
+                        //newlyDispensedDrugs.Drug_Pk = ;
+                        newlyDispensedDrugs.DispensedQuantity = drug.QUANTITY_DISPENSED;
+                        newlyDispensedDrugs.Duration = drug.DURATION;
+                        //todo get frequencyId 
+                        //newlyDispensedDrugs.FrequencyID = drug.FREQUENCY;
+                        newlyDispensedDrugs.SingleDose = Convert.ToDecimal(Regex.Replace(drug.DOSAGE, @"^[A-Za-z]+", ""));
+                        //todo get strength ids
+                        //newlyDispensedDrugs.StrengthID = drug.STRENGTH;
+                        newlyDispensedDrugs.PatientInstructions = drug.DISPENSING_NOTES;
+                        newlyDispensedDrugs.Prophylaxis = 0;
+                    }
+                    foreach (var order in drugDispensed.PHARMACY_ENCODED_ORDER)
+                    {
+                        newlyDispensedDrugs.OrderedQuantity = Convert.ToInt32(order.QUANTITY_PRESCRIBED);
+                        newlyDispensedDrugs.PatientInstructions = order.PRESCRIPTION_NOTES;
+                    }
+                    try
+                    {
+                        _pharmacyDispenseManager.AddPatientPharmacyDispense(newlyDispensedDrugs);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        return Msg = "error " + e.Message;
+                    }
                 }
-
+                var updatedPharmacyOrder = pharmacyOrder;
+                try
+                {
+                    _pharmacyOrderManager.UpdatePharmacyOrder(updatedPharmacyOrder);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return Msg = "error " + e.Message;
+                }
                 Msg = "Success";
             }
             catch (Exception e)
