488a134458b497cf407c518bd7692612c3698ab4
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2018-06-20 14:01:47 +0300

    resolve error on connection string settings for IQCareAPI

--- Solutions/IQCare.Core/IQCare/BuilderExtension.cs
+++ Solutions/IQCare.Core/IQCare/BuilderExtension.cs
@@ -12,15 +12,19 @@ namespace IQCare
 {
     public static class BuilderExtensions
     {
-        public static string ConnectionString { get; set; }
+        public static string _connectionString { get; set; }
         public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration, IConnectionString connectionString)
         {
-            //var dbConnectionString = configuration.GetConnectionString("IQCareConnection");
+            var dbConnectionString = configuration.GetConnectionString("IQCareConnection");
             var iqcareuri = configuration.GetSection("IQCareUri").Get<string>();
             var db = connectionString.GetConnectionString(iqcareuri);
-            ConnectionString = db.Result;
+            _connectionString = db.Result.Replace("\"","").Replace("Application Name=IQCare_EMR;","").Replace("Server", "Data Source").
+                Replace("Type System Version=SQL Data Source 2005;","").Replace("Database", "Initial Catalog")
+                .Replace("Integrated Security=false;", "").Replace("packet size=4128;Min Pool Size=3;Max Pool Size=200;","");
 
-            services.AddDbContext<HtsDbContext>(b => b.UseSqlServer(ConnectionString));
+            _connectionString = _connectionString.Replace(@"\\", @"\");
+
+            services.AddDbContext<HtsDbContext>(b => b.UseSqlServer(_connectionString));
             services.AddScoped(typeof(IHTSRepository<>), typeof(HTSRepository<>));
             services.AddScoped<IHTSUnitOfWork>(c => new HTSUnitOfWork(c.GetRequiredService<HtsDbContext>()));
 
@@ -34,7 +38,7 @@ namespace IQCare
             //var db = connectionString.GetConnectionString(iqcareuri);
             //var dbConnectionString = db.Result;
 
-            services.AddDbContext<CommonDbContext>(b => b.UseSqlServer(ConnectionString));
+            services.AddDbContext<CommonDbContext>(b => b.UseSqlServer(_connectionString));
             services.AddScoped(typeof(ICommonRepository<>), typeof(CommonRepository<>));
             services.AddScoped<ICommonUnitOfWork>(c => new CommonUnitOfWork(c.GetRequiredService<CommonDbContext>()));
 
--- Solutions/IQCare.Core/IQCare/Helpers/ConnectionString.cs
+++ Solutions/IQCare.Core/IQCare/Helpers/ConnectionString.cs
@@ -11,18 +11,21 @@ namespace IQCare.Helpers
         public async Task<string> GetConnectionString(string iqcareUri)
         {
             string connectionString = string.Empty;
-            client.BaseAddress = new Uri(iqcareUri);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("TOKEN", "31LgBzXbDSPj28AwWars8Q==");
-            //client.DefaultRequestHeaders.Accept.Add();
-
-            HttpResponseMessage response = await client.GetAsync("/api/config/connection");
-            if (response.IsSuccessStatusCode)
+            using (client)
             {
-                connectionString = await response.Content.ReadAsStringAsync();
+                client.BaseAddress = new Uri(iqcareUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("TOKEN", "31LgBzXbDSPj28AwWars8Q==");
+
+                HttpResponseMessage response = await client.GetAsync("/api/config/connection");
+                if (response.IsSuccessStatusCode)
+                {
+                    connectionString = await response.Content.ReadAsStringAsync();
+                }
+
+                return connectionString;
             }
-            return connectionString;
         }
     }
 }
