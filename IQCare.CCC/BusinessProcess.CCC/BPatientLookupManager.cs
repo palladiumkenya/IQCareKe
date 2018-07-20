7bc1e6257edc433449068d6dcda13ed3f66f7ed9
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2018-07-20 16:08:00 +0300

    correct pharmacy regimen missmatch for greencard

--- IQCare.CCC/BusinessProcess.CCC/BPatientLookupManager.cs
+++ IQCare.CCC/BusinessProcess.CCC/BPatientLookupManager.cs
@@ -120,7 +120,12 @@ namespace BusinessProcess.CCC
 
                     //expresionFinal = PredicateBuilder.And(expresionFinal, expressionPatientStatus);
                     Expression<Func<PatientLookup, bool>> expressionPatientStatusEnrolled =
-                                    c => c.PatientStatus.ToLower().Contains("active") || c.PatientStatus.ToLower().Contains("death") || c.PatientStatus.ToLower().Contains("losttofollowup") || c.PatientStatus.ToLower().Contains("transfer out") || c.PatientStatus.ToLower().Contains("hiv negative");
+                                    c => c.PatientStatus.ToLower().Contains("active") || 
+                                    c.PatientStatus.ToLower().Contains("death") || 
+                                    c.PatientStatus.ToLower().Contains("losttofollowup") || 
+                                    c.PatientStatus.ToLower().Contains("transfer out") || 
+                                    c.PatientStatus.ToLower().Contains("Confirmed HIV Negative") ||
+                                    c.PatientStatus.ToLower().Contains("hiv negative");
                     expresionFinal = PredicateBuilder.And(expresionFinal, expressionPatientStatusEnrolled);
                     patientLookups = unitOfWork.PatientLookupRepository.Filter(expresionFinal).Take(PredicateBuilder.MaxRecord).ToList();
 
