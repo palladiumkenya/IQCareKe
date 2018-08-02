17a680388bf21fe73ee95f58a4eaa2d3e2522dfc
sosewe <stephenosewe@gmail.com>
2018-08-02 09:10:36 +0300

    integrating visitDetails component

--- Solutions/IQCare.Core/IQCare/frontend/app/pmtct/anc/anc.component.ts
+++ Solutions/IQCare.Core/IQCare/frontend/app/pmtct/anc/anc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-anc',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AncComponent implements OnInit {
 
-  constructor() { }
+  isLinear = false;
+  visitDetailsFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.visitDetailsFormGroup = this._formBuilder.group({
+      visitDate: ['', Validators.required],
+      dateLMP: ['', Validators.required],
+      dateEDD: ['', Validators.required],
+      ageAtMenarche: ['', Validators.required],
+      parityOne: ['', Validators.required],
+      parityTwo: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
 }
