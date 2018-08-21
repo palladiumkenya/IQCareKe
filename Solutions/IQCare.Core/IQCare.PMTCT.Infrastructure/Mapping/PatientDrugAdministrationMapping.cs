47e90fd3a3a150afb986d2b28f419902588d9202
sosewe <stephenosewe@gmail.com>
2018-08-21 07:26:31 +0300

    addingANC previous drug administration model and command

---
+++ Solutions/IQCare.Core/IQCare.PMTCT.Infrastructure/Mapping/PatientDrugAdministrationMapping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQCare.PMTCT.Infrastructure.Mapping
+{
+   public class PatientDrugAdministrationMapping : IEntityTypeConfiguration<PatientProfile>
+    {
+        public void Configure(EntityTypeBuilder<PatientProfile> builder)
+        {
+            builder.ToTable("PatientDrugAdministration")
+                .HasKey(c => c.Id);
+        }
+    }
+}
