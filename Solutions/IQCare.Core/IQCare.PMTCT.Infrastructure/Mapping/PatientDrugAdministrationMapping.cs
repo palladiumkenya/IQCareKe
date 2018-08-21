8225a4bd19ef95179f2a8953eb726dbac2e965b4
sosewe <stephenosewe@gmail.com>
2018-08-21 07:27:37 +0300

    mapping fixes

--- Solutions/IQCare.Core/IQCare.PMTCT.Infrastructure/Mapping/PatientDrugAdministrationMapping.cs
+++ Solutions/IQCare.Core/IQCare.PMTCT.Infrastructure/Mapping/PatientDrugAdministrationMapping.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using IQCare.PMTCT.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace IQCare.PMTCT.Infrastructure.Mapping
 {
-   public class PatientDrugAdministrationMapping : IEntityTypeConfiguration<PatientProfile>
+   public class PatientDrugAdministrationMapping : IEntityTypeConfiguration<PatientDrugAdministration>
     {
-        public void Configure(EntityTypeBuilder<PatientProfile> builder)
+        public void Configure(EntityTypeBuilder<PatientDrugAdministration> builder)
         {
             builder.ToTable("PatientDrugAdministration")
                 .HasKey(c => c.Id);
