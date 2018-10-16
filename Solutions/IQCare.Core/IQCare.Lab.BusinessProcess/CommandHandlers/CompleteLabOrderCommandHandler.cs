cff5b8d3d5adc14b1c3d40cc255bc19732873512
Dro <davidrogola@gmail.com>
2018-10-16 18:16:09 +0300

    Started working on the complete lab order request

--- Solutions/IQCare.Core/IQCare.Lab.BusinessProcess/CommandHandlers/CompleteLabOrderCommandHandler.cs
+++ Solutions/IQCare.Core/IQCare.Lab.BusinessProcess/CommandHandlers/CompleteLabOrderCommandHandler.cs
@@ -1,9 +1,11 @@
 ﻿using IQCare.Lab.BusinessProcess.Commands;
+using IQCare.Lab.Core.Models;
 using IQCare.Lab.Infrastructure.Interface;
 using IQCare.Library;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,7 +22,17 @@ namespace IQCare.Lab.BusinessProcess.CommandHandlers
 
         public Task<Result<CompleteLabOrderResponse>> Handle(CompleteLabOrderCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var totalParameterCount = _labUnitOfwork.Repository<LabTestParameter>().Get(x => x.LabTestId == request.LabTestId).Count();
+
+            var labOrderTestResults = new List<LabOrderTestResult>();
+
+            foreach (var labTestResult in request.LabTestResults)
+            {
+                var labOrderTestResult = new LabOrderTestResult(request.LabOrderTestId, request.LabTestId, labTestResult.ParameterId, labTestResult.ResultValue, labTestResult.ResultOptionId, labTestResult.ResultOption, labTestResult.ResultUnit, labTestResult.ResultOptionId, request.UserId, labTestResult.Undetectable, labTestResult.DetectionLimit);
+
+                labOrderTestResults.Add(labOrderTestResult);
+            }
+            return Task.FromResult(Result<CompleteLabOrderResponse>(new CompleteLabOrderResponse { }));
         }
     }
 }
