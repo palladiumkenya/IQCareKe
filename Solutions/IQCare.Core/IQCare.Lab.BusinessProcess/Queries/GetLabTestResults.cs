1e607ca95c45a0790ccb0e294855c1991e482e37
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2018-10-24 16:00:46 +0300

    complete pnc encounter form

--- Solutions/IQCare.Core/IQCare.Lab.BusinessProcess/Queries/GetLabTestResults.cs
+++ Solutions/IQCare.Core/IQCare.Lab.BusinessProcess/Queries/GetLabTestResults.cs
@@ -14,8 +14,15 @@ namespace IQCare.Lab.BusinessProcess.Queries
     public class LabTestResultViewModel
     {
         public int LabOrderTestId { get; set; }
-        public string LabTestName { get; set; }        public DateTime? SampleDate { get; set; }        public string Reasons { get; set; }
+        public string LabTestName { get; set; }
+        public DateTime? SampleDate { get; set; }
+        public string Reasons { get; set; }
         public string Results { get; set; }
-        public string ResultTexts { get; set; }        public int LabTestId { get; set; }        public decimal ResultValues { get; set; }        public string ResultUnits { get; set; }        public string ResultOptions { get; set; }        public DateTime? ResultDate { get; set; }
+        public string ResultTexts { get; set; }
+        public int LabTestId { get; set; }
+        public decimal ResultValues { get; set; }
+        public string ResultUnits { get; set; }
+        public string ResultOptions { get; set; }
+        public DateTime? ResultDate { get; set; }
     }
 }
