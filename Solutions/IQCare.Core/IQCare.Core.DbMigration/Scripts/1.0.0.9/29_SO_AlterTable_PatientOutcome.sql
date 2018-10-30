9efb385417e345ed8c80dfe6c1a797c2b43610d7
Dro <davidrogola@gmail.com>
2018-10-30 10:50:15 +0300

    Merged script to create Identity column into one file

--- Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/29_SO_AlterTable_PatientOutcome.sql
+++ Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/29_SO_AlterTable_PatientOutcome.sql
@@ -13,3 +13,18 @@ BEGIN
     -- Column Exists
 	ALTER TABLE dbo.PatientOutcome DROP COLUMN OutcomeId;
 END
+
+
+BEGIN TRANSACTION 
+ALTER TABLE PatientOutcome ADD Id_new INT IDENTITY(1, 1) NOT NULL 
+GO
+
+ALTER TABLE PatientOutcome DROP CONSTRAINT  [PK_PatientOutCome_Id]
+ALTER TABLE PatientOutcome DROP COLUMN Id
+GO
+
+EXEC sp_rename 'PatientOutcome.Id_new', 'Id', 'Column'
+
+ALTER TABLE PatientOutcome ADD CONSTRAINT FK_PatientOutcome_Id PRIMARY KEY(Id);
+
+COMMIT TRANSACTION
