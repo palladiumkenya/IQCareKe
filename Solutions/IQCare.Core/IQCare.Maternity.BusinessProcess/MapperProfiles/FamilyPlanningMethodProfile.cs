c223759e4e62d83530cc324930e90d84bbcf429f
bernardadero <aderobernard@gmail.com>
2018-10-31 17:45:22 +0300

    Add Family Planning

---
+++ Solutions/IQCare.Core/IQCare.Maternity.BusinessProcess/MapperProfiles/FamilyPlanningMethodProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using IQCare.Maternity.BusinessProcess.Commands.PNC;
+using IQCare.Maternity.Core.Domain.PNC;
+
+namespace IQCare.Maternity.BusinessProcess.MapperProfiles
+{
+    public class FamilyPlanningMethodProfile : Profile
+    {
+        public FamilyPlanningMethodProfile()
+        {
+            CreateMap<AddPatientFamilyPlanningCommand, PatientFamilyPlanning>();
+
+        }
+    }
+}
