15d04faedb24f55481f5cdac1d39bae9de7579b5
Dro <davidrogola@gmail.com>
2018-11-02 17:18:22 +0300

    Added pmtct scripts to the db migration project

---
+++ Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.8/PMTCT/03_CreateTable_PatientDelivery.sql
@@ -0,0 +1,24 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PatientDelivery]') AND type in (N'U')) 
+BEGIN
+CREATE TABLE [dbo].[PatientDelivery](
+	[DeliveryID] [int] IDENTITY(1,1) NOT NULL,
+	[PatientMasterVisitID] [int] NOT NULL,
+	[ProfileID] [int] NOT NULL,
+	[DurationOfLabour] [int] NULL,
+	[DateOfDelivery] [datetime] NULL,
+	[TimeOfDelivery] [time](7) NULL,
+	[ModeOfDelivery] [int] NULL,
+	[PlacentaComplete] [int] NULL,
+	[BloodLoss] [int] NULL,
+	[MotherCondition] [int] NULL,
+	[DeliveryComplications] [int] NULL,
+	[DeliveryComplicationNotes] [varchar](max) NULL,
+	[DeliveryConductedBy] [varchar](50) NULL,
+	[DeleteFlag] [bit] NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[AuditData] [varchar](max) NULL,
+  CONSTRAINT [PK_PatientDelivery] PRIMARY KEY CLUSTERED ([DeliveryID] ASC),
+  CONSTRAINT [FK_PatientDelivery_PatientMasterVisit] FOREIGN KEY([PatientMasterVisitID]) REFERENCES [dbo].[PatientMasterVisit] ([Id])
+ )
+END
