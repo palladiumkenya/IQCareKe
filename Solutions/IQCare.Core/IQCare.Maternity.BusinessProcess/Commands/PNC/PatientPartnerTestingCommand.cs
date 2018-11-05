580de0c925bf06802a77bb99a6301c0aeb2d4062
bernardadero <aderobernard@gmail.com>
2018-11-05 10:45:01 +0300

    PatientPartnerTesting

---
+++ Solutions/IQCare.Core/IQCare.Maternity.BusinessProcess/Commands/PNC/PatientPartnerTestingCommand.cs
@@ -0,0 +1,22 @@
+﻿using IQCare.Library;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQCare.Maternity.BusinessProcess.Commands.PNC
+{
+    public class PatientPartnerTestingCommand : IRequest<Result<PatientPatnerTestingResponse>>
+    {
+        public int PatientId { get; set; }
+        public int PatientMasterVisitId { get; set; }
+        public int PartnerTested { get; set; }
+        public int PartnerHIVResult { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int CreatedBy { get; set; }
+        public Boolean DeleteFlag { get; set; }
+        public string AuditData { get; set; }
+    }
+    public class PatientPatnerTestingResponse
+    { public int PatientId { get; set; } }
+}
--- Solutions/IQCare.Core/IQCare.Maternity.BusinessProcess/QueryHandlers/GetPatientFamilyPlanningMethodQueryHandler.cs
+++ Solutions/IQCare.Core/IQCare.Maternity.BusinessProcess/QueryHandlers/GetPatientFamilyPlanningMethodQueryHandler.cs
@@ -10,24 +10,38 @@ using System.Threading.Tasks;
 using IQCare.Library;
 using IQCare.Maternity.BusinessProcess.Queries.PNC;
 using AutoMapper;
+using IQCare.Maternity.Core.Domain.PNC;
 
 namespace IQCare.Maternity.BusinessProcess.QueryHandlers
 {
-    //class GetPatientFamilyPlanningMethodQueryHandler : IRequestHandler<GetPatientFamilyPlanningMethodQuery, Result<List<PatientFamilyPlanningMethodViewModel>>>
-    //{
-    //    IMaternityUnitOfWork _maternityUnitOfWork;
-    //    IMapper _mapper;
-    //    ILogger logger = Log.ForContext<GetPatientDiagnosisInfoQueryHandler>();
+    class GetPatientFamilyPlanningMethodQueryHandler : IRequestHandler<GetPatientFamilyPlanningMethodQuery, Result<List<PatientFamilyPlanningMethodViewModel>>>
+    {
+        IMaternityUnitOfWork _maternityUnitOfWork;
+        IMapper _mapper;
+        ILogger logger = Log.ForContext<GetPatientDiagnosisInfoQueryHandler>();
 
-    //    public GetPatientFamilyPlanningMethodQueryHandler(IMaternityUnitOfWork maternityUnitOfWork, IMapper mapper)
-    //    {
-    //        _maternityUnitOfWork = maternityUnitOfWork;
-    //        _mapper = mapper;
-    //    }
-    //    //public Task<Result<List<PatientFamilyPlanningMethodViewModel>>> Handle(GetPatientFamilyPlanningMethodQuery request, CancellationToken cancellationToken)
-    //    //{
-           
+        public GetPatientFamilyPlanningMethodQueryHandler(IMaternityUnitOfWork maternityUnitOfWork, IMapper mapper)
+        {
+            _maternityUnitOfWork = maternityUnitOfWork;
+            _mapper = mapper;
+        }
+        public Task<Result<List<PatientFamilyPlanningMethodViewModel>>> Handle(GetPatientFamilyPlanningMethodQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var patientFamilyPlanning = _maternityUnitOfWork.Repository<PatientFamilyPlanningMethodView>().Get(x => x.PatientId == request.PatientId)
+                        .AsEnumerable();
+                var familyPlanningViewModel = _mapper.Map<List<PatientFamilyPlanningMethodViewModel>>(patientFamilyPlanning);
 
-    //    //}
-    //}
+
+                return Task.FromResult(Result<List<PatientFamilyPlanningMethodViewModel>>.Valid(familyPlanningViewModel));
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"An error occured while getting patient family planning methods{ request.PatientId}");
+                return Task.FromResult(Result<List<PatientFamilyPlanningMethodViewModel>>.Invalid(ex.Message));
+            }
+
+        }
+    }
 }
