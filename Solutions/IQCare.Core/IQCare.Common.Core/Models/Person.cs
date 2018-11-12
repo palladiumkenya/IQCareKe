fb0c2d49ef05b5494384e71d21e8608cebfbb6a0
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2018-11-12 11:24:48 +0300

    merge conflict

--- Solutions/IQCare.Core/IQCare.Common.Core/Models/Person.cs
+++ Solutions/IQCare.Core/IQCare.Common.Core/Models/Person.cs
@@ -19,7 +19,7 @@ namespace IQCare.Common.Core.Models
         public DateTime CreateDate { get; set; }
         public int CreatedBy { get; set; }
         public string AuditData { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public bool? DobPrecision { get; set; }
         public DateTime? RegistrationDate { get; set; }
         public int? FacilityId { get; set; }
