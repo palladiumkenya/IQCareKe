8c325c421c847f3dc600061f89228ae6f2fe4bbd
chesterosoronyas <chester.osoro@thepalladiumgroup.com>
2018-11-30 16:39:43 +0300

    changes on icdcodes resolve the errors

--- Scripts/MIGRATIONSI/8.ICDCodes/2.LookupTableFlag.sql
+++ Scripts/MIGRATIONSI/8.ICDCodes/2.LookupTableFlag.sql
@@ -11,8 +11,17 @@ IF NOT EXISTS (
 BEGIN
 ALTER TABLE PatientDiagnosis ADD LookupTableFlag   bit  null
 END
+go
 
---select  * from PatientDiagnosis
 
+
+IF  EXISTS (
+  SELECT * 
+  FROM   sys.columns 
+  WHERE  object_id = OBJECT_ID(N'[dbo].[PatientDiagnosis]') 
+         AND name = 'LookupTableFlag'
+)
+BEGIN
 update PatientDiagnosis set LookupTableFlag='1'  
 where LookupTableFlag is null
+END
