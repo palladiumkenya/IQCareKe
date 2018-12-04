98c598c7f12faaaf0c64b1f8e712846a01a25c21
sosewe <stephenosewe@gmail.com>
2018-12-04 19:26:44 +0300

    Database changes

--- IQCare.CCC/Entities.CCC/Triage/PatientVital.cs
+++ IQCare.CCC/Entities.CCC/Triage/PatientVital.cs
@@ -20,17 +20,17 @@ namespace Entities.CCC.Triage
         public int PatientMasterVisitId { get; set; }
         [ForeignKey("PatientMasterVisitId")]
         public virtual PatientMasterVisit PatientMasterVisit { get; set; }
-        public decimal Temperature { get; set; }
-        public decimal RespiratoryRate { get; set; }
-        public decimal HeartRate { get; set; }
-        public int Bpdiastolic { get; set; }
-        public int BpSystolic { get; set; }
+        public decimal? Temperature { get; set; }
+        public decimal? RespiratoryRate { get; set; }
+        public decimal? HeartRate { get; set; }
+        public int? Bpdiastolic { get; set; }
+        public int? BpSystolic { get; set; }
         public decimal Height { get; set; }
         public decimal Weight { get; set; }
-        public decimal  Muac { get; set; }
-        public decimal SpO2 { get; set; }
+        public decimal?  Muac { get; set; }
+        public decimal? SpO2 { get; set; }
         public decimal BMI { get; set; }
-        public decimal HeadCircumference { get; set; }
+        public decimal? HeadCircumference { get; set; }
         public string BMIZ { get; set; }
         public string WeightForHeight { get; set; }
         public string WeightForAge { get; set; }
