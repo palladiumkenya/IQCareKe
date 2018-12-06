cdd91831e2c47fbb6a3a09e0f241e72423f83c94
Dro <davidrogola@gmail.com>
2018-12-06 17:50:37 +0300

    Added new component to add lab order details

--- Solutions/IQCare.Core/IQCare/frontend/app/clinical/lab/lab-order/lab-order.component.ts
+++ Solutions/IQCare.Core/IQCare/frontend/app/clinical/lab/lab-order/lab-order.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { LabTestInfo, AddLabOrderCommand } from '../../_models/AddLabOrderCommand';
+import { LaborderService } from '../../_services/laborder.service';
+import { SnotifyService } from 'ng-snotify';
+import { NotificationService } from '../../../shared/_services/notification.service';
 
 @Component({
   selector: 'app-lab-order',
@@ -16,7 +20,10 @@ labTestData : any[];
 
 labTestReasonOptions : any[];
 
-  constructor(private formBuilder :FormBuilder) {
+  constructor(private formBuilder :FormBuilder, 
+    private labOrderService: LaborderService,
+    private  snotifyService : SnotifyService,
+    private notificationService: NotificationService) {
 
      this.labOrderFormGroup = this.formBuilder.group({
       labTestId: new FormControl('', [Validators.required]),
@@ -43,6 +50,58 @@ labTestReasonOptions : any[];
       orderReasonId: this.labOrderFormGroup.get('labtestReasonId').value.itemId,
       testNotes: this.labOrderFormGroup.get('labTestNotes').value
     });
+  }
+
+
+  public SubmitOrder() {
+    if(this.labOrderFormGroup.invalid)
+        return;
+
+     const labTestInfo : LabTestInfo [] = [];  
+    
+     this.labTestData.forEach(x=>{
+      labTestInfo.push({
+        Id : x.testId,
+        Notes : x.testNotes,
+        LabTestName :x.test
+      });
+
+      const labOrderCommand : AddLabOrderCommand = {
+      Ptn_Pk: 0,
+      PatientId : 5,
+      LocationId : 5,
+      FacilityId : 4,
+      VisitId : 3,
+      ModuleId : 3,
+      OrderedBy : 2,
+      OrderDate : this.labOrderFormGroup.get('orderDate').value,
+      ClinicalOrderNotes :  this.labOrderFormGroup.get('orderNotes').value,
+      CreateDate :new Date,
+      OrderStatus : "Pending",
+      UserId : 2,
+      PatientMasterVisitId : 3,
+      LastTests : this.labTestData
+      }
+
+      this.labOrderService.addLabOrder(labOrderCommand).subscribe(
+        res=>{
+          console.log(`Add lab order info`);
+          console.log(res);
+          this.snotifyService.success('Successfully added lab order ', 'Lab', this.notificationService.getConfig());
+      }, 
+      (err)=>
+      {
+        console.log("An error occured while adding lab order command "+err);
+        this.snotifyService.error('Error saving lab order ' + err, 'Lab', this.notificationService.getConfig());
+      },
+      ()=>{
+        this.snotifyService.success('Lab order details added successfully', 'Lab',
+        this.notificationService.getConfig());
+        this.labOrderFormGroup.reset();
+      });
+
+     })
+    
   }
 
 }
