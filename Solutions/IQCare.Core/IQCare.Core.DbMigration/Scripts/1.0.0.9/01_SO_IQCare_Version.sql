d4c1b2ab428a82da1a54aeb9619125b735bcd596
sosewe <stephenosewe@gmail.com>
2018-12-07 16:50:33 +0300

    version update

--- Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/01_SO_IQCare_Version.sql
+++ Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/01_SO_IQCare_Version.sql
@@ -3,6 +3,6 @@
 			UPDATE AppAdmin
 			SET 
 			 AppVer= 'Ver 2.0.0 Kenya HMIS',
-			 VersionName = 'Kenya HMIS Ver 2.0.0-BETA',
+			 VersionName = 'Kenya HMIS Ver 2.0.0 BETA',
 			 DBVer= 'Ver 2.0.0 Kenya HMIS',
 			 RelDate='04-Dec-2018'
