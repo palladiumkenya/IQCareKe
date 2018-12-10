2185d49273b8ef66785c94962f676447f0ceb2a5
Dro <davidrogola@gmail.com>
2018-12-10 18:24:45 +0300

    completed happy path for adding lab order pending validation and UI clean up

--- Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/2.0.0/12_DO_Insert_Clinical_Service_Area.sql
+++ Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/2.0.0/12_DO_Insert_Clinical_Service_Area.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO [dbo].[ServiceArea]([Name],[Code],[DisplayName],[CreatedBy],[CreateDate],[DeleteFlag],[AuditData])
-     VALUES('Clinical' ,'Clinical',1 ,GETDATE(),0,NULL)
+     VALUES('Clinical' ,'CLN','Clinical',1 ,GETDATE(),0,NULL)
 GO
