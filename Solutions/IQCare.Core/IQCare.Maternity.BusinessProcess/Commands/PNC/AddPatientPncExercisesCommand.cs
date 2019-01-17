c031dc1de3aa501ad6ae0636c20ab0ee01b2f478
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2019-01-17 12:07:43 +0300

    saving for pnc exercises and remove referred from, to as required fields

---
+++ Solutions/IQCare.Core/IQCare.Maternity.BusinessProcess/Commands/PNC/AddPatientPncExercisesCommand.cs
@@ -0,0 +1,22 @@
+﻿using IQCare.Library;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQCare.Maternity.BusinessProcess.Commands.PNC
+{
+    public class AddPatientPncExercisesCommand : IRequest<Result<AddPatientPncExercisesResponse>>
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int PatientMasterVisitId { get; set; }
+        public int PncExercisesDoneId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class AddPatientPncExercisesResponse
+    {
+        public int PncExercisesId { get; set; }
+    }
+}
