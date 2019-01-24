471cfe81df1ff2e02589babc6fb410833b348f57
sosewe <stephenosewe@gmail.com>
2019-01-24 09:05:50 +0300

    chang to iqcare version

--- Scripts/1.0.0.8/07 Data/IQCareVersion.sql
+++ Scripts/1.0.0.8/07 Data/IQCareVersion.sql
@@ -2,7 +2,7 @@
 UPDATE [dbo].[AppAdmin] SET 
 	AppVer='Ver 2.0.0 Kenya HMIS',
 	DBVer='Ver 2.0.0 Kenya HMIS',
-	RelDate='2019-01-10 00:00:00.000',
+	RelDate='2019-01-24 00:00:00.000',
 	VersionName='Kenya HMIS Ver 2.0.0 BETA'
 
 	WHERE Id>0
--- Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/01_SO_IQCare_Version.sql
+++ Solutions/IQCare.Core/IQCare.Core.DbMigration/Scripts/1.0.0.9/01_SO_IQCare_Version.sql
@@ -5,7 +5,7 @@
 			 AppVer= 'Ver 2.0.0 Kenya HMIS',
 			 VersionName = 'Kenya HMIS Ver 2.0.0 BETA',
 			 DBVer= 'Ver 2.0.0 Kenya HMIS',
-			 RelDate='2019-01-10 00:00:00.000'
+			 RelDate='2019-01-24 00:00:00.000'
 			 WHERE Id>0
 
 
