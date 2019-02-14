95945181d16bf03a053ca19aa7f0aef1981e612a
Osoro, Chester <Chester.Osoro@thepalladiumgroup.com>
2019-02-14 12:44:31 +0300

    changes to include controller to fetch form details and values

---
+++ Solutions/IQCare.Core/IQCare.AIR.BusinessProcess/Command/GetFormValueCommand.cs
@@ -0,0 +1,39 @@
+﻿using IQCare.Library;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQCare.AIR.BusinessProcess.Command
+{
+   public  class GetFormValueCommand:IRequest<Result<GetFormValueResponse>>
+    {
+       public int Id { get; set; }
+
+       
+    }
+    public class GetFormValueResponse
+{
+        public List<ReportingValues> reportingValues { get; set; }
+}
+
+    public class ReportingValues
+    {
+        public int ReportingFormId { get; set; }
+
+        public int ReportingId { get; set; }
+
+        public DateTime ReportDate { get; set; }
+
+        public int IndicatorId { get; set; }
+
+
+        public int ReportingPeriodId { get; set; }
+
+        public decimal ResultNumeric { get; set; }
+
+        public string ResultText { get; set; }
+
+
+    }
+}
---
+++ Solutions/IQCare.Core/IQCare.AIR.BusinessProcess/CommandHandlers/GetFormValueCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IQCare.AIR.BusinessProcess.Command;
+using IQCare.Library;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace IQCare.AIR.BusinessProcess.CommandHandlers
+{
+    public class GetFormValueCommandHandler:IRequestHandler<GetFormValueCommand,Result<GetFormValueResponse>>
+    {
+
+    }
+}
