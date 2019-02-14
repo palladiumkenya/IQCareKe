6846400925d6839f5515ee85daa9471a48e80760
sosewe <stephenosewe@gmail.com>
2019-02-14 13:36:06 +0300

    Adding IQCare.Common.web APIService Endpoint

---
+++ Solutions/IQCare.Core/IQCare.Common.BusinessProcess/Commands/Matrix/MatrixCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IQCare.Common.BusinessProcess.Commands.Setup;
+using IQCare.Common.Core.Models;
+using IQCare.Library;
+using MediatR;
+
+namespace IQCare.Common.BusinessProcess.Commands.Matrix
+{
+    public class MatrixCommand : IRequest<Result<EmrMatrixResponse>>
+    {
+      
+    }
+
+    public class EmrMatrixResponse
+    {
+        public DateTime? LastLoginDate { get; set; }
+        public string EmrVersion { get; set; }
+        public string EmrName { get; set; }
+        public DateTime? LastMoH731RunDate { get; set; }
+    }
+}
---
+++ Solutions/IQCare.Core/IQCare.Common.BusinessProcess/Services/EmrMatrixService.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IQCare.Common.Core.Models;
+using IQCare.Common.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace IQCare.Common.BusinessProcess.Services
+{
+    public class EmrMatrixService
+    {
+        private readonly ICommonUnitOfWork _unitOfWork;
+
+        public EmrMatrixService(ICommonUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<EmrMatrix> GetCurrentEmrMatrix()
+        {
+            try
+            {
+                EmrMatrix currentMatrix = await _unitOfWork.Repository<EmrMatrix>().Get().FirstOrDefaultAsync();
+                return currentMatrix;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return null;
+            }
+        }
+
+    }
+}
