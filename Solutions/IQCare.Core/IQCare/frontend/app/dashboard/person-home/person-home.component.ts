adf66dad2795ad3968e9c4bfe1400453885336fd
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2019-11-28 16:30:32 +0300

    resolve shadowing error

--- Solutions/IQCare.Core/IQCare/frontend/app/dashboard/person-home/person-home.component.ts
+++ Solutions/IQCare.Core/IQCare/frontend/app/dashboard/person-home/person-home.component.ts
@@ -77,11 +77,8 @@ export class PersonHomeComponent implements OnInit {
             const { ExitReasonsArray } = res;
 
             const { HTSEncounterHistoryArray } = res;
-            console.log('Resource loaded');
-            console.log(RiskAssessmentArray);
             this.careenddetails = await this.personService.getPatientByPersonId(this.personId).pipe(mergeMap(
-                res => this.personService.getPatientCareEndedHistory(res['patientId'])
-
+                response => this.personService.getPatientCareEndedHistory(response['patientId'])
             )).toPromise();
 
             this.htshistory = HTSEncounterHistoryArray;
